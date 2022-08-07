65e5e7594593b3721d5e23f3fbe6b8e6e0fb82ce
hoanguyen <vanhoaltw@gmail.com>
2022-08-07 11:37:09 +0700

    onmic bot

--- index.js
+++ index.js
@@ -1,34 +1,19 @@
-const Telegram = require('./src/telegram')
-const Trello = require('./src/Trello')
-const config = require('./config.json')
+const Telegram = require("./src/telegram");
+const Trello = require("./src/Trello");
 
-const telegram = new Telegram({
-  key: config.telegram_key,
-  chat_id: config.telegram_chat_id
-})
+require("dotenv").config()
 
-const trello = new Trello({
-  token: config.trello_token,
-  key: config.trello_key,
-  board: config.board
-})
-
-telegram.on('change.memberCount', (err, data) => {
-  if (!err) {
-    trello.updateMemberCount(data.count)
-  }
-})
-telegram.on('new.poll', (err, data, cb) => {
-  if (!err) {
-    trello.addNewPoll(data, cb)
-  }
-})
-telegram.on('change.tally', (polls) => {
-  trello.updatePollCards(polls)
-})
-telegram.on('change.closePoll', (poll) => {
-  trello.updateOneCard(poll)
-})
-telegram.on('change.deletePoll', (poll) => {
-  trello.deletePollCard(poll)
-})
+new Telegram({
+  key: process.env.TELEBOT_TOKEN,
+  chat_id: process.env.TELECHAT_ID,
+  trelloBot: new Trello({
+    key: process.env.TRELLO_KEY,
+    token: process.env.TRELLO_TOKEN,
+    board: {
+      board_name: process.env.BOARD_NAME,
+      board_id: process.env.BOARD_ID,
+      idLabels: process.env.ID_LABELS,
+      idMembers: process.env.ID_MEMEBERS,
+    },
+  }),
+});
